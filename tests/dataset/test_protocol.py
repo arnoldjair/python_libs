9539d89f4910b601685879142cdf9f2ecab8cb85
Arnold Jiménez <arnoldjair@gmail.com>
2022-07-21 18:00:16 -0500

    feature(protocol): load_dotenv removed in test

--- tests/dataset/test_protocol.py
+++ tests/dataset/test_protocol.py
@@ -1,13 +1,10 @@
 import os
 
-from dotenv import load_dotenv
-
 from python_libs.dataset import Protocol
 
 
 class TestProtocol:
     def test_get_test_protocol(self):
-        load_dotenv(os.path.join(os.getcwd(), ".env"))
         json_path = os.path.join(os.getcwd(), "configs", "unit_test.json")
         train, test = Protocol.get_protocol(json_path)
         assert train is not None
