9db767eb18e22c04d1a4ac8f3eb72dabc981c0c5
Arnold Jiménez <arnoldjair@gmail.com>
2022-07-21 17:56:37 -0500

    feature(video): video tests

--- tests/video/test_video_util.py
+++ tests/video/test_video_util.py
@@ -3,7 +3,7 @@ import os
 from pymediainfo import MediaInfo
 
 from python_libs.video import get_video_info_file
-from python_libs.video.video_util import get_video_info
+from python_libs.video.video_util import get_frames
 
 
 class TestVideoUtil:
@@ -20,5 +20,5 @@ class TestVideoUtil:
     def test_get_frames(self):
         path = os.path.join(os.getcwd(), "data", "raw", "G_NT_5s_wg_E_10_1.mp4")
         print(MediaInfo.parse(path))
-        frames = get_video_info(path)
+        frames = get_frames(path)
         assert frames is not None
