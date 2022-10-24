21d11c1e4fe9d6b28ea0f8ab4e2c762628366848
Arnold Jiménez <arnoldjair@gmail.com>
2022-10-24 18:37:21 -0500

    feature(datasource): Add pairs datasource for siamese training

--- src/python_libs/dataset/datasource.py
+++ src/python_libs/dataset/datasource.py
@@ -1,8 +1,6 @@
 """
 DataSource
 """
-import random
-
 import torch
 from torch.utils.data import Dataset
 
@@ -14,32 +12,12 @@ class Datasource(Dataset):
         Dataset (Records): Datasource
     """
 
-    def __init__(self, dataset):
+    def __init__(self, dataset, time=300, width=416, height=416):
         super().__init__()
         self.dataset = dataset
 
     def __getitem__(self, index):
-        # We need approx 50 % of  records of the same class
-        same_class = random.randint(0, 1)
-        record_0 = self.dataset[index]
-        label_0 = record_0.label
-        label = 0
-        if same_class:
-            while True:
-                # keep looping till the same class record is found
-                index_1 = random.randint(0, self.__len__() - 1)
-                record_1 = self.dataset[index_1]
-                label_1 = record_1.label
-                if label_0 == label_1:
-                    label = 1
-                    break
-        else:
-            while True:
-                index_1 = random.randint(0, self.__len__() - 1)
-                record_1 = self.dataset[index_1]
-                label_1 = record_1.label
-                if label_0 != label_1:
-                    break
+        record_0, record_1, label = self.dataset[index]
 
         return (
             torch.from_numpy(record_0.load(time=300)).float(),
